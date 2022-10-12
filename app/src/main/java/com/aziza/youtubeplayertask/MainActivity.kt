c040217bb57a2ecc9ea5272352bd97b764a366b8
Aziza Helmy <aziza.helmy35@gmail.com>
2022-10-12 10:47:50 +0200

    show simple video

--- app/src/main/java/com/aziza/youtubeplayertask/MainActivity.kt
+++ app/src/main/java/com/aziza/youtubeplayertask/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.aziza.youtubeplayertask
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
-import androidx.media3.common.Player
 import androidx.media3.common.util.Util
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
@@ -23,7 +18,8 @@ class MainActivity : AppCompatActivity() {
         PlayerAdapter(this)
     }
     private var playList = ArrayList<VideoPlayer>()
-    //private val playbackStateListener: Player.Listener = playbackStateListener()
+
+  private val playbackStateListener: Player.Listener = playbackStateListener()
     private var player: ExoPlayer? = null
     private var player2: ExoPlayer? = null
     private var playWhenReady = true
@@ -34,32 +30,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initialzeRecyclerView()
+//        initializePlayer(getString(R.string.media_url_mp4))
+//        playList.add(VideoPlayer(player!!))
+//        //playList.add(VideoPlayer(player2!!))
+//        playList.add(VideoPlayer(player!!))
+//        // playList.add(VideoPlayer(player2!!))
+//        playList.add(VideoPlayer(player!!))
+
+    }
+
+    private fun initializePlayer(uri: String) {
+        val trackSelector = DefaultTrackSelector(this).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())
+        }
         player = ExoPlayer.Builder(this)
-           // .setTrackSelector(trackSelector)
-            .build()
-            .also { exoPlayer ->
-               // binding.playerView.player = exoPlayer
-
-                val mediaItem = MediaItem.Builder()
-                    .setUri(getString(R.string.media_url_mp4))
-                    // .setMimeType(MimeTypes.APPLICATION_MPD)
-                    .build()
-                exoPlayer.setMediaItem(mediaItem)
-
-                exoPlayer.playWhenReady = false // i will play it
-                exoPlayer.seekTo(currentItem, playbackPosition)
-               // exoPlayer.addListener(playbackStateListener)
-                exoPlayer.prepare()
-            }
-        player2 = ExoPlayer.Builder(this)
-            // .setTrackSelector(trackSelector)
+             .setTrackSelector(trackSelector)
             .build()
             .also { exoPlayer ->
                 // binding.playerView.player = exoPlayer
 
                 val mediaItem = MediaItem.Builder()
-                    .setUri(getString(R.string.media_url_mp3))
-                    // .setMimeType(MimeTypes.APPLICATION_MPD)
+                    .setUri(uri)
                     .build()
                 exoPlayer.setMediaItem(mediaItem)
 
@@ -69,9 +60,9 @@ class MainActivity : AppCompatActivity() {
                 exoPlayer.prepare()
             }
         playList.add(VideoPlayer(player!!))
-        playList.add(VideoPlayer(player2!!))
         playList.add(VideoPlayer(player!!))
-        playList.add(VideoPlayer(player2!!))
+        playList.add(VideoPlayer(player!!))
+        playList.add(VideoPlayer(player!!))
         playList.add(VideoPlayer(player!!))
 
     }
@@ -82,14 +73,14 @@ class MainActivity : AppCompatActivity() {
                 GridLayoutManager(this.context, 1, GridLayoutManager.HORIZONTAL, false)
             setLayoutManager(layoutManager)
             playerAdapter.submitLis(playList)
-            adapter=playerAdapter
+            adapter = playerAdapter
         }
     }
 
     public override fun onStart() {
         super.onStart()
         if (Util.SDK_INT > 23) {
-            //initializePlayer()
+            initializePlayer(getString(R.string.media_url_mp4))
         }
     }
 
@@ -97,14 +88,14 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         //hideSystemUi()
         if (Util.SDK_INT <= 23 || player == null) {
-           // initializePlayer()
+            initializePlayer(getString(R.string.media_url_mp4))
         }
     }
 
     public override fun onPause() {
         super.onPause()
         if (Util.SDK_INT <= 23) {
-           // releasePlayer()
+            initializePlayer(getString(R.string.media_url_mp4))
         }
     }
 
@@ -115,58 +106,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    private fun initializePlayer() {
-//        val trackSelector = DefaultTrackSelector(this).apply {
-//            setParameters(buildUponParameters().setMaxVideoSizeSd())
-//        }
-//        player = ExoPlayer.Builder(this)
-//            .setTrackSelector(trackSelector)
-//            .build()
-//            .also { exoPlayer ->
-//                binding.playerView.player = exoPlayer
-//
-//                val mediaItem = MediaItem.Builder()
-//                    .setUri(getString(R.string.media_url_mp4))
-//                    // .setMimeType(MimeTypes.APPLICATION_MPD)
-//                    .build()
-//                exoPlayer.setMediaItem(mediaItem)
-//
-//                exoPlayer.playWhenReady = false // i will play it
-//                exoPlayer.seekTo(currentItem, playbackPosition)
-//                exoPlayer.addListener(playbackStateListener)
-//                exoPlayer.prepare()
-//            }
-//    }
-
-//    private fun releasePlayer() {
-//        player?.let { exoPlayer ->
-//            playbackPosition = exoPlayer.currentPosition
-//            currentItem = exoPlayer.currentMediaItemIndex
-//            playWhenReady = exoPlayer.playWhenReady
-//            exoPlayer.removeListener(playbackStateListener)
-//            exoPlayer.release()
-//        }
-//        player = null
-//    }
-
-  //  @SuppressLint("InlinedApi")
-//    private fun hideSystemUi() {
-//        WindowCompat.setDecorFitsSystemWindows(window, false)
-//        WindowInsetsControllerCompat(window, binding.playerView).let { controller ->
-//            controller.hide(WindowInsetsCompat.Type.systemBars())
-//            controller.systemBarsBehavior =
-//                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-//        }
-//    }
-
-//        override fun onPlaybackStateChanged(playbackState: Int) {
-//            val stateString: String = when (playbackState) {
-//                ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
-//                ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
-//                ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
-//                ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
-//                else -> "UNKNOWN_STATE             -"
-//            }
-//            Log.d("TAG", "changed state to $stateString")
-//        }
-    }
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            val stateString: String = when (playbackState) {
+                ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
+                ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
+                ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
+                ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
+                else -> "UNKNOWN_STATE             -"
+            }
+            Log.d("TAG", "changed state to $stateString")
+        }
+}
